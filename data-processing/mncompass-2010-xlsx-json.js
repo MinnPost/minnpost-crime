6eb3af49d670ff110b150bb1f2dbb4c861195dd1
Alan Palazzolo <apalazzolo@minnpost.com>
2013-04-17 17:45:55 -0500

    Rough draft of mn demographic conversion from xlsx to csv.

---
+++ data-processing/mncompass-2010-xlsx-json.js
@@ -0,0 +1,181 @@
+/**
+ * This is a script to convert the MN Compass demographic data in MS Excel
+ * format into json format to be used in other applications.
+ * 
+ * Input (a CSV version of the xlsx)
+ * data/demographics/2010/MNC_2011_NeighborhoodProfiles_AllData_NoSuppressionR2.csv
+ * 
+ * Output:
+ * data/demographics/2010/twin-cities-neighborhood-demographics-2010.json
+ *
+ */
+var path = require('path');
+var fs = require('fs');
+var office = require('csv');
+var _ = require('underscore');
+
+
+// Top level variables
+var input = 'data/demographics/2010/MNC_2011_NeighborhoodProfiles_AllData_NoSuppressionR2.csv';
+var input_path = path.resolve(__dirname, '../' + input);
+var output = 'data/demographics/2010/twin-cities-neighborhood-demographics-2010.json';
+var output_path = path.resolve(__dirname, '../' + output);
+var cData = [];
+var cHeaders = [];
+var fData = {};
+var dataStartRow = 4;
+var columnDataStart = 5;
+
+
+
+// Make ID from headings
+function makeID(name, type, number, year) {
+  id = name.toLowerCase().replace(/[^a-z0-9]/g, '_');
+  id += (type) ? ('-' + type.toLowerCase().replace(/[^a-z0-9]/g, '_')) : '';
+  id += (number) ? ('-' + number.toLowerCase().replace(/[^a-z0-9]/g, '_')) : '';
+  id += (year) ? ('-' + year) : '';
+  return id;
+};
+
+// Make place_id
+function makePlaceID(neighborhood, community) {
+  if (!neighborhood) {
+    neighborhood = 'community_' + community.toLowerCase().replace(/[^a-z0-9]/g, '_');
+  }
+  else {
+    neighborhood = neighborhood.toLowerCase().replace(/[^a-z0-9]/g, '_')
+      .replace('__', '_').replace('__', '_');
+  }
+  
+  return neighborhood;
+};
+
+
+// Handle data values
+function makeValue(data, parseNum) {
+  data = (_.isString(data) && data == '-') ? null : data;
+  data = (_.isString(data) && data.toLowerCase() == 'x') ? null : data;
+  
+  if (parseNum && data !== null) {
+    data = parseFloat(data.replace(/[^\d\.\-]/g, ''));
+  }
+  
+  return data;
+};
+
+// Make type smaller for smaller filesize
+function makeType(type) {
+  type = (type.toLowerCase() === 'number') ? '#' : type;
+  type = (type.toLowerCase() === 'percent') ? '%' : type;
+  return type;
+};
+
+// Make column headers and metadata
+function makeHeaders() {
+  _.each(cData[dataStartRow - 1], function(c, i) {
+    var columnMetaData = {};
+    
+    columnMetaData.n = cData[3][i];
+      
+    if (i >= columnDataStart) {
+      columnMetaData.id = makeID(cData[3][i], cData[0][i], cData[1][i], cData[2][i]);
+      // Type
+      columnMetaData.t = makeType(cData[1][i]);
+      // Year
+      columnMetaData.y = cData[2][i];
+    }
+    else {
+      columnMetaData.id = makeID(cData[3][i]);
+    }
+    
+    cHeaders.push(columnMetaData);
+  });
+};
+
+// Save out data
+function saveNewFile() {
+  fs.writeFile(output_path, JSON.stringify(fData), function(error) {
+    if (error) {
+      console.log('Error saving: ' + error);
+    } 
+    else {
+      console.log('JSON saved.');
+    }
+  }); 
+};
+
+// Process data once we have the header data
+function processCSV() {
+  var csvProcessor = csv()
+    .from.stream(fs.createReadStream(input_path))
+    .transform(function(row) {
+      // Trim data and only use rows that are not empty
+      row = _.map(row, function(column, i) {
+        column = (_.isString(column)) ? column.trim() : column;
+        column = makeValue(column, (i >= columnDataStart));
+        return column;
+      });
+      
+      if (row[columnDataStart] != '') {
+        return row;
+      }
+    })
+    .on('record', function(row, index) {
+      var processedRow = {};
+      
+      if (index >= dataStartRow) {
+        _.each(cHeaders, function(h, i) {
+          var meta = _.clone(h);
+          delete meta.id;
+        
+          processedRow[h.id] = meta;
+          processedRow[h.id].value = row[i];
+        });
+      
+        // Make place/neighborhood id
+        processedRow.p_id = {
+          name: 'Place ID',
+          value: makePlaceID(row[1], row[2])
+        };
+        
+        fData[processedRow.p_id.value] = processedRow;
+      }
+    })
+    .on('end', function(count) {
+      saveNewFile();
+    })
+    .on('error', function(error) {
+      console.log('Error: ' + error.message);
+    });
+};
+
+// The first pass we use to make column names
+function processCSVHeaders() {
+  var csvProcessor = csv()
+    .from.stream(fs.createReadStream(input_path))
+    .transform(function(row) {
+      // Trim data and only use rows that are not empty
+      row = _.map(row, function(column) {
+        column = (_.isString(column)) ? column.trim() : column;
+        return column;
+      });
+      
+      if (row[columnDataStart] != '') {
+        return row;
+      }
+    })
+    .on('record', function(row, index) {
+      if (index < dataStartRow) {
+        cData.push(row);
+      }
+    })
+    .on('end', function(count) {
+      makeHeaders();
+      processCSV();
+    })
+    .on('error', function(error) {
+      console.log('Error: ' + error.message);
+    });
+};
+
+processCSVHeaders();
