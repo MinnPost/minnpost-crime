37724edb2e5a0bd0cd29ae88afcb192c0bcba490
Alan Palazzolo <apalazzolo@minnpost.com>
2013-04-18 16:05:57 -0500

    Updating build.

--- dist/minnpost-crime.latest.js
+++ dist/minnpost-crime.latest.js
@@ -1,5 +1,5 @@
-var DATAPATH = DATAPATH || '../data/';
 var mapper = {};
+var neighborhood2011GeoJSON;
 
 (function($, undefined) {
   var crimeURLBase = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&callback=?&query=';
@@ -9,6 +9,7 @@ var mapper = {};
   var crimes = {};
   var neighborhoods = {};
   var geojson;
+  var templateTooltip = _.template($('#template-tooltip-crime').html());
   
   // Handle incoming data
   function drawMap(type) {
@@ -32,17 +33,21 @@ var mapper = {};
     });
     
     $('#crime-map').html('');
+    
+    neighborhood2011GeoJSON = geojson;
+    
     var map1 = SimpleMapD3({
       container: '#crime-map',
       data: geojson,
       colorOn: true,
-      colorProperty: type
+      colorProperty: type,
+      tooltipContent: templateTooltip
     });
   }
 
   
   mapper = function(dataPath) {
-    dataPath = dataPath || './data/';
+    dataPath = dataPath || './data/neighborhoods/minneapolis/';
   
     // Get data
     $.jsonp({
@@ -55,7 +60,7 @@ var mapper = {};
         if (dataPath.indexOf('proxy') > 0) {
           method = 'jsonp';
         }
-        var neighborhoodTopoJSON = dataPath + 'neighborhoods/minneapolis/minneapolis-neighborhoods-2012-keyed.topo.json';
+        var neighborhoodTopoJSON = dataPath + 'minneapolis-neighborhoods-2012-keyed.topo.json';
         $.ajax({
           dataType: method,
           url: neighborhoodTopoJSON,
@@ -166,3 +171,112 @@ var mapper = {};
     
   });
 })(jQuery, window);
+
+var demographics = {};
+
+
+(function($, undefined) {
+  var demoData;
+  var geojson;
+  var tooltipTemplate = $('#template-tooltip-demographics').html();
+  var seriesTypes = ['total', 'homicide', 'rape', 'robbery', 'agg_assault', 
+    'burglary', 'larceny', 'auto_theft', 'arson'];
+  
+  // Handle incoming data
+  function matchDemographics() {
+    if (neighborhood2011GeoJSON && demoData) {
+      geojson = neighborhood2011GeoJSON;
+      
+      _.each(demoData.data, function(d, r) {
+        _.each(demoData.data[r], function(c, k) {
+          if (demoData.meta[k] && demoData.meta[k].type == '%') {
+            demoData.data[r][k] = c / 100;
+          }
+        });
+      });
+      
+      geojson.features = _.map(geojson.features, function(f) {
+        f.properties = _.extend(f.properties, demoData.data['minneapolis_' + f.properties.neighbor_1]);
+        return f;
+      });
+      
+      _.each(seriesTypes, function(d) {
+        $('#demographics-select-1').append($('<option value="' + d + '">' + d + '</option>'));
+        $('#demographics-select-2').append($('<option value="' + d + '">' + d + '</option>'));
+      });
+      
+      _.each(demoData.meta, function(d) {
+        if (d.data != 'MARGIN OF ERROR') {
+          $('#demographics-select-1').append($('<option value="' + d.key + '">' + 
+            d.name + ' (' + d.type + ' ' + d.data + ')</option>'));
+          $('#demographics-select-2').append($('<option value="' + d.key + '">' + 
+            d.name + ' (' + d.type + ' ' + d.data + ')</option>'));
+        }
+      });
+    }
+  }
+  
+  
+  // Draw data
+  function drawDemographics(demo1, demo2) {
+    if (neighborhood2011GeoJSON && demoData && demo1 && demo2) {
+      
+      // Draw chart
+      
+      // Draw maps
+      var template = _.template(tooltipTemplate
+        .replace('{{{PROP1}}}', demo1)
+        .replace('{{{PROP2}}}', demo2)
+        .replace('{{{PROP1}}}', demo1)
+        .replace('{{{PROP2}}}', demo2));
+      
+      $('#demographic-map-1').html('');
+      var map1 = SimpleMapD3({
+        container: '#demographic-map-1',
+        data: geojson,
+        colorOn: true,
+        colorProperty: demo1,
+        tooltipContent: template
+      });
+      $('#demographic-map-2').html('');
+      var map2 = SimpleMapD3({
+        container: '#demographic-map-2',
+        data: geojson,
+        colorOn: true,
+        colorProperty: demo2,
+        tooltipContent: template
+      });
+    }
+  }
+
+  
+  demographics = function(dataPath) {
+    dataPath = dataPath || './data/demographics/2010/';
+    
+    // Get data
+    var method = 'json';
+    if (dataPath.indexOf('proxy') > 0) {
+      method = 'jsonp';
+    }
+    var url = dataPath + 'twin-cities-neighborhood-demographics-2010.json';
+    $.ajax({
+      dataType: method,
+      url: url,
+      success: function(data) {
+        demoData = data;
+        matchDemographics();
+      }
+    });
+    
+    $('#demographics-submit').on('click', function(e) {
+      e.preventDefault();
+      drawDemographics($('#demographics-select-1').val(), $('#demographics-select-2').val());
+    });
+    
+    $('#demographics-load-data').on('click', function(e) {
+      e.preventDefault();
+      matchDemographics();
+    });
+  };
+  
+})(jQuery);
