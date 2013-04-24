f7c4b784850efbc904d26b61cead1b8fbb3a55b4
Alan Palazzolo <apalazzolo@minnpost.com>
2013-04-24 15:07:46 -0500

    Adding in ability to see chart for years.

--- js/charts.js
+++ js/charts.js
@@ -4,7 +4,8 @@
 (function($, w, undefined) {
   $(document).ready(function() {
     var neighborhoodListURL = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&query=SELECT%20DISTINCT%20neighborhood_key%20FROM%20swdata%20ORDER%20BY%20neighborhood_key&callback=?";
-    var neighborhoodURL = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&query=SELECT%20*%20FROM%20%60swdata%60%20WHERE%20neighborhood_key%20%3D%20'[[[NEIGHBORHOOD]]]'&callback=?";
+    var neighborhoodURL = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&query=SELECT * FROM `swdata` WHERE neighborhood_key = '[[[NEIGHBORHOOD]]]'&callback=?";
+    var neighbothoodURLYear = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&query=SELECT year, 1 AS month, [[[COLUMNS]]] FROM `swdata` WHERE neighborhood_key = '[[[NEIGHBORHOOD]]]' AND YEAR NOT IN (2002, 2013) GROUP BY year&callback=?";
     
     // Compiling templates to be efficient
     var templates = {
@@ -15,15 +16,28 @@
     $('#chart-example').html(templates.loading({ }));
     
     // Handle new neighborhood
-    var updateChart = function(neighborhood, max) {
+    var updateChart = function(neighborhood, max, by) {
       $('#chart-example').html(templates.loading({ }));
       
+      var url = neighborhoodURL;
+      var seriesTypes = ['total', 'homicide', 'rape', 'robbery', 'agg_assault', 
+        'burglary', 'larceny', 'auto_theft', 'arson'];
+      
+      if (by === 'year') {
+        var columns = [];
+        _.each(seriesTypes, function(s) {
+          columns.push('SUM(' + s + ') AS ' + s);
+        });
+        url = neighbothoodURLYear.replace('[[[COLUMNS]]]', columns.join(', '));
+      }
+      
+      url = encodeURI(url.replace('[[[NEIGHBORHOOD]]]', neighborhood));
+      
+      max = (max == 'auto') ? null : parseInt(max, 10);
+      
       $.jsonp({
-        url: neighborhoodURL.replace('[[[NEIGHBORHOOD]]]', neighborhood),
+        url: url,
         success: function(data) {
-          var seriesTypes = ['homicide', 'rape', 'robbery', 'agg_assault', 
-            'burglary', 'larceny', 'auto_theft', 'arson'];
-            
           var series = [];
           
           _.each(seriesTypes, function(t) {
@@ -54,10 +68,8 @@
               max: max
             },
             plotOptions: {
-              spline: {
-                marker: {
-                  enabled: false
-                }
+              column: {
+                stacking: 'normal'
               }
             },
             series: series
@@ -79,8 +91,16 @@
         });
         $('#chart-example').html('<p><br /><br />Select neighborhood...</p>');
         
+        var updateChartAction = function(e) {
+          e.preventDefault();
+          updateChart($('#neighborhood-select').val(), $('#max-select').val() || 50, $('#chart-by-select').val());
+        };
+        
         $('#neighborhood-select').on('change', function(e) {
-          updateChart($(this).val(), parseInt($('#max-select').val(), 10) || 50);
+          updateChartAction(e);
+        });
+        $('.update-chart').on('click', function(e) {
+          updateChartAction(e);
         });
       }
     });
