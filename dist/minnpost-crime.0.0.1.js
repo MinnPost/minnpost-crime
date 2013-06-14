8f61e0c337c1f1217cff68b6043330bc7afa7e75
Alan Palazzolo <apalazzolo@minnpost.com>
2013-06-14 12:08:10 -0500

    Updating build.

--- dist/minnpost-crime.0.0.1.js
+++ dist/minnpost-crime.0.0.1.js
@@ -1,282 +1,177 @@
-var mapper = {};
-var neighborhood2011GeoJSON;
-
-(function($, undefined) {
-  var crimeURLBase = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&callback=?&query=';
-  var crimeQuery = "SELECT * FROM `swdata` WHERE year = '2011'";
-  var seriesTypes = ['homicide', 'rape', 'robbery', 'agg_assault', 
-    'burglary', 'larceny', 'auto_theft', 'arson', 'total'];
-  var crimes = {};
-  var neighborhoods = {};
-  var geojson;
-  var templateTooltip = _.template($('#template-tooltip-crime').html());
-  
-  // Handle incoming data
-  function drawMap(type) {
-    type = type || 'total';
-    if (!geojson) {
-      geojson = topojson.feature(neighborhoods, neighborhoods.objects['minneapolis-neighborhoods-2012-keyed.geo']);
-    }
-    
-    // Add total crimes up
-    geojson.features = _.map(geojson.features, function(f) {
-      var crimesInNeighborhood = _.where(crimes, { 'neighborhood_key': f.properties.neighbor_1 });
-      
-      // Get totals of crime groups
-      _.each(seriesTypes, function(p) {
-        var total = 0;
-        _.each(crimesInNeighborhood, function(c) { total += c[p]; });
-        f.properties[p] = total;
-      });
-      
-      return f;
-    });
-    
-    $('#crime-map').html('');
-    
-    neighborhood2011GeoJSON = geojson;
-    
-    var map1 = SimpleMapD3({
-      container: '#crime-map',
-      data: geojson,
-      colorOn: true,
-      colorProperty: type,
-      tooltipContent: templateTooltip
-    });
-  }
-
-  
-  mapper = function(dataPath) {
-    dataPath = dataPath || './data/neighborhoods/minneapolis/';
-  
-    // Get data
-    $.jsonp({
-      url: crimeURLBase + encodeURI(crimeQuery),
-      success: function(data) {
-        crimes = data;
-        
-        // hackish
-        var method = 'json';
-        if (dataPath.indexOf('proxy') > 0) {
-          method = 'jsonp';
-        }
-        var neighborhoodTopoJSON = dataPath + 'minneapolis-neighborhoods-2012-keyed.topo.json';
-        $.ajax({
-          dataType: method,
-          url: neighborhoodTopoJSON,
-          success: function(data) {
-            neighborhoods = data;
-            drawMap();
-          }
-        });
-      }
-    });
-    
-    
-    $('#crime-type-select').on('change', function(e) {
-      drawMap($(this).val() || 'total');
-    });
-  };
-  
-})(jQuery);
+/**
+ * Some core functionality for minnpost-crime
+ */
 
 /**
- * Highcharts
+ * Global variable to handle some things
+ * like templates.
  */
-(function($, w, undefined) {
-  $(document).ready(function() {
-    var neighborhoodListURL = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&query=SELECT%20DISTINCT%20neighborhood_key%20FROM%20swdata%20ORDER%20BY%20neighborhood_key&callback=?";
-    var neighborhoodURL = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minneapolis_aggregate_crime_data&query=SELECT%20*%20FROM%20%60swdata%60%20WHERE%20neighborhood_key%20%3D%20'[[[NEIGHBORHOOD]]]'&callback=?";
-    
-    // Compiling templates to be efficient
-    var templates = {
-      loading: _.template($('#template-loading').html())
-    };
-    
-    // Mark as loading while getting data
-    $('#chart-example').html(templates.loading({ }));
-    
-    // Handle new neighborhood
-    var updateChart = function(neighborhood, max) {
-      $('#chart-example').html(templates.loading({ }));
-      
-      $.jsonp({
-        url: neighborhoodURL.replace('[[[NEIGHBORHOOD]]]', neighborhood),
-        success: function(data) {
-          var seriesTypes = ['homicide', 'rape', 'robbery', 'agg_assault', 
-            'burglary', 'larceny', 'auto_theft', 'arson'];
-            
-          var series = [];
-          
-          _.each(seriesTypes, function(t) {
-            series.push({
-              name: t,
-              data: _.map(data, function(d) {
-                return [
-                  Date.UTC(d.year, d.month, 1),
-                  parseInt(d[t], 10)
-                ];
-              })
-            });
-          });
-          
-          var exampleChart = new Highcharts.Chart({
-            chart: {
-              renderTo: 'chart-example',
-              type: 'spline'
-            },
-            title: {
-              text: 'Neighborhood crime data'
-            },
-            xAxis: {
-              type: 'datetime'
-            },
-            yAxis: {
-              min: 0,
-              max: max
-            },
-            plotOptions: {
-              spline: {
-                marker: {
-                  enabled: false
-                }
-              }
-            },
-            series: series
-          });
-          
-        },
-        error: function(e) {
-          console.log('error');
-        }
-      });
-    };
-    
-    // Get list
-    $.jsonp({
-      url: neighborhoodListURL,
-      success: function(data) {
-        _.each(data, function(d) {
-          $('#neighborhood-select').append($('<option value="' + d.neighborhood_key + '">' + d.neighborhood_key + '</option>'));
-        });
-        $('#chart-example').html('<p><br /><br />Select neighborhood...</p>');
-        
-        $('#neighborhood-select').on('change', function(e) {
-          updateChart($(this).val(), parseInt($('#max-select').val(), 10) || 50);
-        });
-      }
-    });
-    
-    
-    
-  });
-})(jQuery, window);
+var mpApp = mpApp || {};
+mpApp['minnpost-crime'] = mpApp['minnpost-crime'] || {};
 
-var demographics = {};
-
-
-(function($, undefined) {
-  var demoData;
-  var geojson;
-  var tooltipTemplate = $('#template-tooltip-demographics').html();
-  var seriesTypes = ['total', 'homicide', 'rape', 'robbery', 'agg_assault', 
-    'burglary', 'larceny', 'auto_theft', 'arson'];
-  
-  // Handle incoming data
-  function matchDemographics() {
-    if (neighborhood2011GeoJSON && demoData) {
-      geojson = neighborhood2011GeoJSON;
-      
-      _.each(demoData.data, function(d, r) {
-        _.each(demoData.data[r], function(c, k) {
-          if (demoData.meta[k] && demoData.meta[k].type == '%') {
-            demoData.data[r][k] = c / 100;
-          }
-        });
-      });
-      
-      geojson.features = _.map(geojson.features, function(f) {
-        f.properties = _.extend(f.properties, demoData.data['minneapolis_' + f.properties.neighbor_1]);
-        return f;
-      });
-      
-      _.each(seriesTypes, function(d) {
-        $('#demographics-select-1').append($('<option value="' + d + '">' + d + '</option>'));
-        $('#demographics-select-2').append($('<option value="' + d + '">' + d + '</option>'));
-      });
-      
-      _.each(demoData.meta, function(d) {
-        if (d.data != 'MARGIN OF ERROR') {
-          $('#demographics-select-1').append($('<option value="' + d.key + '">' + 
-            d.name + ' (' + d.type + ' ' + d.data + ')</option>'));
-          $('#demographics-select-2').append($('<option value="' + d.key + '">' + 
-            d.name + ' (' + d.type + ' ' + d.data + ')</option>'));
-        }
-      });
+/**
+ * Extend underscore
+ */
+_.mixin({
+  /**
+   * Formats number into currency
+   */
+  formatCurrency: function(num) {
+    var rgx = (/(\d+)(\d{3})/);
+    split = num.toFixed(2).toString().split('.');
+    while (rgx.test(split[0])) {
+      split[0] = split[0].replace(rgx, '$1' + ',' + '$2');
     }
+    return '$' + split[0] + '.' + split[1];
+  },
+  
+  /**
+   * Formats percentage
+   */
+  formatPercentage: function(num) {
+    return (num * 100).toFixed(1).toString() + '%';
   }
+});
   
-  
-  // Draw data
-  function drawDemographics(demo1, demo2) {
-    if (neighborhood2011GeoJSON && demoData && demo1 && demo2) {
-      
-      // Draw chart
-      
-      // Draw maps
-      var template = _.template(tooltipTemplate
-        .replace('{{{PROP1}}}', demo1)
-        .replace('{{{PROP2}}}', demo2)
-        .replace('{{{PROP1}}}', demo1)
-        .replace('{{{PROP2}}}', demo2));
-      
-      $('#demographic-map-1').html('');
-      var map1 = SimpleMapD3({
-        container: '#demographic-map-1',
-        data: geojson,
-        colorOn: true,
-        colorProperty: demo1,
-        tooltipContent: template
-      });
-      $('#demographic-map-2').html('');
-      var map2 = SimpleMapD3({
-        container: '#demographic-map-2',
-        data: geojson,
-        colorOn: true,
-        colorProperty: demo2,
-        tooltipContent: template
-      });
-    }
-  }
+/**
+ * Override Backbone's ajax function to use $.jsonp as it handles
+ * errors for JSONP requests
+ */
+if (_.isFunction(Backbone.$.jsonp)) {
+  Backbone.ajax = function() {
+    return Backbone.$.jsonp.apply(Backbone.$, arguments);
+  };
+}
 
-  
-  demographics = function(dataPath) {
-    dataPath = dataPath || './data/demographics/2010/';
-    
-    // Get data
-    var method = 'json';
-    if (dataPath.indexOf('proxy') > 0) {
-      method = 'jsonp';
-    }
-    var url = dataPath + 'twin-cities-neighborhood-demographics-2010.json';
-    $.ajax({
-      dataType: method,
-      url: url,
-      success: function(data) {
-        demoData = data;
-        matchDemographics();
-      }
-    });
-    
-    $('#demographics-submit').on('click', function(e) {
-      e.preventDefault();
-      drawDemographics($('#demographics-select-1').val(), $('#demographics-select-2').val());
-    });
-    
-    $('#demographics-load-data').on('click', function(e) {
-      e.preventDefault();
-      matchDemographics();
-    });
+/**
+ * Non global
+ */
+(function(app, $, undefined) {
+  app.defaultOptions = {
+    dataPath: './'
   };
   
-})(jQuery);
+  /**
+   * Template handling.  For development, we want to use
+   * the template files directly, but for build, they should be
+   * compiled into JS.
+   *
+   * See JST grunt plugin to understand how templates
+   * are compiled.
+   *
+   * Expects callback like: function(compiledTemplate) {  }
+   */
+  app.templates = app.templates || {};
+  app.getTemplate = function(name, callback, context) {
+    var templatePath = 'js/templates/' + name + '.html';
+    context = context || app;
+    
+    if (!_.isUndefined(app.templates[templatePath])) {
+      callback.apply(context, [ app.templates[templatePath] ]);
+    }
+    else {
+      $.ajax({
+        url: templatePath,
+        method: 'GET',
+        async: false,
+        contentType: 'text',
+        success: function(data) {
+          app.templates[templatePath] = _.template(data);
+          callback.apply(context, [ app.templates[templatePath] ]);
+        }
+      });
+    }
+  };
+  
+  /**
+   * Data source handling.  For development, we can call
+   * the data directly from the JSON file, but for production
+   * we want to proxy for JSONP.
+   *
+   * `name` should be relative path to dataset minus the .json
+   *
+   * Returns jQuery's defferred object.
+   */
+  app.data = app.data || {};
+  app.getData = function(name) {
+    var proxyPrefix = 'http://mp-jsonproxy.herokuapp.com/proxy?callback=?&url=';
+    var useJSONP = false;
+    var defers = [];
+    
+    name = (_.isArray(name)) ? name : [ name ];
+    
+    // If the data path is not relative, then use JSONP
+    if (app.options && app.options.dataPath.indexOf('http') === 0) {
+      useJSONP = true;
+    }
+    
+    // Go through each file and add to defers
+    _.each(name, function(d) {
+      var defer;
+      
+      if (useJSONP) {
+        defer = $.jsonp({
+          url: proxyPrefix + encodeURI(app.options.dataPath + d + '.json')
+        });
+      }
+      else {
+        defer = $.getJSON(app.options.dataPath + d + '.json');
+      }
+      
+      defers.push(defer);
+    });
+    
+    return $.when.apply($, defers);
+  };
+})(mpApp['minnpost-crime'], jQuery);
+
+/**
+ * Main application container for the Legislature Tracker
+ *
+ * An 'e' prefix is referring to editorialized content.
+ */
+(function(app, $, undefined) {
+
+  app.Application = Backbone.Router.extend({
+    routes: {
+      'city/:city': 'routeCity',
+      'neighborhood/:city/:neighborhood': 'routeNeighborhood',
+      '*defaultR': 'routeDefault'
+    },
+  
+    initialize: function(options) {
+      // Set app options
+      app.options = _.extend(app.defaultOptions, options);
+    
+      // Bind to help with some event callbacks
+      _.bindAll(this);
+      
+      this.start();
+    },
+    
+    // Start application (after data has been loaded),
+    // specifically start Backbone history
+    start: function() {
+      Backbone.history.start();
+    },
+  
+    // Default route
+    routeDefault: function() {
+      this.navigate('/city/minneapolis', { trigger: true, replace: true });
+    },
+  
+    // City route
+    routeCity: function(city) {
+    },
+  
+    // City route
+    neighborhood: function(city, neighborhood) {
+    }
+  });
+  
+  // Wrapper function to start application
+  app.start = function(options) {
+    app.router = new app.Application(options);
+    return app;
+  };
+})(mpApp['minnpost-crime'], jQuery);
